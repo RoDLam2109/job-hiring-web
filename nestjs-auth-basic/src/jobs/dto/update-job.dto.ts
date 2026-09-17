ad2852ab91f2dfaf18f0212b8ae8448827b77544
Lmao <Lam210905@gmail.com>
2026-09-17 22:09:02 +0700

    Fix job DTO import casing

--- nestjs-auth-basic/src/jobs/dto/update-job.dto.ts
+++ nestjs-auth-basic/src/jobs/dto/update-job.dto.ts
@@ -1,4 +1,4 @@
-import { CreateJobDto } from './create-Job.dto';
+import { CreateJobDto } from './create-job.dto';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class UpdateJobDto extends PartialType(CreateJobDto) {
